7bbcd29e7d46f6f0aaec927a5099ee11e6ab9a33
younu-Yun <dbssnfjd122@gmail.com>
2023-03-14 21:34:03 +0900

    Add clock

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.getElementById('clock');
+
+function getClock() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = String(date.getHours()).padStart(2, "0");
+    const min = String(date.getMinutes()).padStart(2, "0");
+    const sec = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hour}:${min}:${sec}`;
+
+}
+
+setInterval(getClock, 1000);
