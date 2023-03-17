10206d9198ec149252876fb6f92eba56cb560e60
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-03-18 00:00:17 +0900

    save Todos in LocalStorage

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const todoForm = document.getElementById("todoForm");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todoList");
 
+let todos = [];
+
+const TODOS_KEY = "todos";
+
+function saveTodo() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 function deleteTodo(e) {
     const parentLi = e.target.parentElement;
     parentLi.remove();
@@ -26,7 +34,17 @@ function todoSubmitHandler(e) {
     e.preventDefault();
     let inputValue = todoInput.value;
     todoInput.value = "";
+    todos.push(inputValue);
+
     paintTodo(inputValue);
+    saveTodo();
 }
 
 todoForm.addEventListener("submit", todoSubmitHandler);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (localStorage.getItem(TODOS_KEY) !== null) {
+    let parsedTodos = JSON.parse(savedTodos);
+    parsedTodos.forEach((item) => paintTodo(item));
+}
